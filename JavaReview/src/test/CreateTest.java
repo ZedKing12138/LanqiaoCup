3ed11d6aa6a399a853a6166ed72bd1d6dde739a2
“yoyonana0991 <u201816996@hust.edu.cn>
2021-02-20 18:17:54 +0800

    创建了JavaReview项目

---
+++ JavaReview/src/test/CreateTest.java
@@ -0,0 +1,12 @@
+package test;
+public class CreateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String string1=new String("love yoona forever！");	
+		for (int i = 0; i < 100; i++) {
+		System.out.println(string1);
+		}
+
+	}
+}
