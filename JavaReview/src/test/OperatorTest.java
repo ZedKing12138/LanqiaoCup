d461e4e3dda80a18af7eaebec6c632e28dc77ac5
“yoyonana0991 <u201816996@hust.edu.cn>
2021-02-22 15:38:39 +0800

    add something new

---
+++ JavaReview/src/test/OperatorTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class OperatorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float f = 	1.2656f;
+		System.out.print(f/0);
+	}
+
+}
---
+++ JavaReview/src/test/PrimitiveTypeTest.java
@@ -0,0 +1,19 @@
+package test;
+
+public class PrimitiveTypeTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+ 	byte a = 125;
+	short b = 12563;
+	int c = 13133;
+	long d = 1313132;
+	float f = 1.25654f;
+	double e = 12.31313;
+	char g = '芜';
+	boolean h = true;
+	System.out.println(g);
+	}
+
+}
